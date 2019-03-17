afcf173807e687b757babe972377685c2c7d50ba
TimGeissler <tim.d.geissler@gmail.com>
2019-03-17 21:00:52 +0200

    Fix sign up login comparison & timetable window init.

--- src/signupFrame/SignupController.java
+++ src/signupFrame/SignupController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Button;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class SignupController {
 
@@ -19,7 +20,7 @@ public class SignupController {
     private Button signupButton;
 
     @FXML
-    private Button closeButton;
+    private Button cancelButton;
 
     @FXML
     private JFXTextField usernameField;
@@ -28,19 +29,39 @@ public class SignupController {
     private JFXPasswordField passwordField;
 
     @FXML
-    void handleButtonClicks(ActionEvent event) {
+    void handleButtonClicks(ActionEvent event) throws Exception {
         if (event.getSource() == signupButton) {
             //Signup button clicked, read information from text fields
             System.out.println("Signup clicked");
             String username = usernameField.getText(), password = passwordField.getText();
+            System.out.println("Username: " + username + "\nPassword: " + password);
             //Check if username is already present
             //Columns to return
             String [] columns = {"Username"};
-            //Rows to return
-            int rows = 1;
+            ArrayList<ArrayList<String>> queryResult = Functions.selectQuery("SELECT * FROM users_table WHERE Username = '" + username + "';", columns);
+            if (queryResult.size() > 1) {
+                //query has returned results, there is a matching username
+                System.out.println("Username already present...");
+                //Show alert dialog
+                Functions.setAlertMessage("There is already a user with this username");
+                functions.loadStage("/notificationFrame/notification.fxml", "Error");
+            } else if (password.length() < 8) {
+                //New username, may be added to database
+                System.out.println("Password not long enough");
+                //Show alert dialog
+                Functions.setAlertMessage("Password is not long enough");
+                functions.loadStage("/notificationFrame/notification.fxml", "Error");
+            } else {
+                //New username & password long enough, may be added to database
+                System.out.println("Unique username, password long enough - may be inserted");
+                Functions.insdelQuery("INSERT INTO users_table (Username, Password) VALUES (" + username + ", " + password + ");");
+                System.out.println("Details saved");
+            }
+            /*
             try {
-                ArrayList<ArrayList<String>> queryResult = functions.selectQuery("SELECT * FROM users_table WHERE Username = '" + username + "';", columns);
-                if (queryResult.get(0).get(0) == username) {
+                ArrayList<ArrayList<String>> queryResult = Functions.selectQuery("SELECT * FROM users_table WHERE Username = '" + username + "';", columns);
+                //Use null-safe comparison
+                if (Objects.equals(queryResult.get(0).get(0), username)) {
                     //Pre-existing username
                     System.out.println("Username already present...");
                     //Show alert dialog
@@ -55,8 +76,8 @@ public class SignupController {
             } catch (Exception e) {
                 e.printStackTrace();
                 System.out.println("Error in select query, Exception caught");
-            }
-        } else if (event.getSource() == "closeButton") {
+            }*/
+        } else if (event.getSource() == cancelButton) {
             //Close window
             ((Node) (event.getSource())).getScene().getWindow().hide();
         }
--- src/timetableFrame/TimetableController.java
+++ src/timetableFrame/TimetableController.java
@@ -1,96 +1,159 @@
 package timetableFrame;
 
 import com.jfoenix.controls.JFXComboBox;
+import com.root.DatabaseSettings;
 import com.root.Functions;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-import java.awt.event.ActionEvent;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class TimetableController implements Initializable {
 
     Functions functions = new Functions();
+    DatabaseSettings db = new DatabaseSettings();
+
     @FXML
     private JFXComboBox<String> studentsList;
+
     @FXML
     private Label lessonLabel;
+
     @FXML
     private Label lessonLabel1;
+
     @FXML
     private Label lessonLabel2;
+
     @FXML
     private Label lessonLabel3;
+
     @FXML
     private Label lessonLabel4;
+
     @FXML
     private Label lessonLabel5;
+
     @FXML
     private Label lessonLabel6;
+
     @FXML
     private Label lessonLabel7;
+
     @FXML
     private Label lessonLabel8;
+
     @FXML
     private Label lessonLabel9;
+
     @FXML
     private Label lessonLabel10;
+
     @FXML
     private Label lessonLabel11;
+
     @FXML
     private Label lessonLabel12;
+
     @FXML
     private Label lessonLabel13;
+
     @FXML
     private Label lessonLabel14;
+
     @FXML
     private Label lessonLabel15;
+
     @FXML
     private Label lessonLabel16;
+
     @FXML
     private Label lessonLabel17;
+
     @FXML
     private Label lessonLabel18;
+
     @FXML
     private Label lessonLabel19;
+
     @FXML
     private Label lessonLabel20;
+
     @FXML
     private Label lessonLabel21;
+
     @FXML
     private Label lessonLabel22;
+
     @FXML
     private Label lessonLabel23;
+
     @FXML
     private Label lessonLabel24;
+
     @FXML
     private Label lessonLabel25;
+
     @FXML
     private Label lessonLabel26;
+
     @FXML
     private Label lessonLabel27;
+
     @FXML
     private Label lessonLabel28;
+
     @FXML
     private Label lessonLabel29;
+
     @FXML
     private Button closeButton;
+
     @FXML
     private Button exportButton;
+
     @FXML
     private Button calculateButton;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        System.out.println("Timetable controller initialized...");
-    }
 
     @FXML
     void handleButtonClicks(ActionEvent event) {
+        if (event.getSource() == closeButton) {
+            System.out.println("Close timetableFrame");
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        } else if (event.getSource() == exportButton) {
+            System.out.println("Export timetable");
+        } else if (event.getSource() == calculateButton) {
+            System.out.println("Calculate timetable");
+        }
+    }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("TimetableFrame Initialised");
+        //Set dropdown menu text
+        ArrayList<String> studentList = new ArrayList<>();
+        String [] columns = {"StudentName"};
+        String students_table = db.getStudentTable(), student_teacher = db.getStudentTeacherTable();
+        String selectQuery = "SELECT " + students_table + ".StudentName FROM " + students_table + " INNER JOIN " +
+                student_teacher + " ON " + student_teacher + ".StudentID = " + students_table + ".StudentID WHERE " +
+                student_teacher + ".TeacherID = '" + functions.getUsername() + "';";
+        System.out.println("students_table = " + students_table + "\nstudent_teacher = " + student_teacher);
+        ArrayList<ArrayList<String>> queryResult = Functions.selectQuery(selectQuery, columns);
+        System.out.println("SQL SELECT Result - > " + queryResult);
+        if (queryResult.size() == 1) {
+            System.out.println("No students found...Add students");
+        } else {
+            System.out.println("Iterating through SQL result ArrayList...");
+            for (int i = 0; i < queryResult.size(); i++) {
+                studentList.add(queryResult.get(0).get(i));
+            }
+        }
     }
 }
